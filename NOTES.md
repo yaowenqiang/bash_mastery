8831a0c22b4dfddd41b4a343d4ba14b6d1604b51
yaowenqiang <yaowenqiang111@163.com>
2026-04-16 23:44:50 +0800

    add more position parameters demo

--- NOTES.md
+++ NOTES.md
@@ -386,4 +386,5 @@ Step 5: Redirections
 
 
 
+> echo "$(ls *.txt)"
 
---
+++ position_parameters.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo My name is  $1
+echo My home directory is  $2
+echo My favourite color is  $3
+
+echo "My name is  $1"
+echo "My home directory is  $2"
+echo "My favourite color is  $3"
+
+echo "the nineth argument is $9"
+echo "the 10th argument is $10"
+echo "the 11th argument is $11"
+
+echo "the 10th argument is {$10}"
+echo "the 11th argument is {$11}"
