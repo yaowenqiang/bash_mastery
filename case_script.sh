e0d08f7a2d6ebd9f8a7831fe92117348f28a1f2b
yaowenqiang <yaowenqiang111@163.com>
2026-04-18 16:07:59 +0800

    add case demo

---
+++ case_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Please enter a number: " number
+
+case "$number" in
+  [0-9]) echo 'you have entered a single digit number' ;;
+  [0-9][0-9]) echo 'you have entered two digit numbers' ;;
+  [0-9][0-9][0-9]) echo 'you have entered three digit numbers' ;;
+  *)echo "you have enter a number that is more then three digits";;
+esac
+
